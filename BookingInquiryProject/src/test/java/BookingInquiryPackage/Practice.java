8409bfd989c900fcefea4d9980cad74e54386b3d
narman <narman@TSSMAATES07095.PILSHIP.COM>
2023-12-27 16:19:24 +0530

    Initial Commit

---
+++ BookingInquiryProject/src/test/java/BookingInquiryPackage/Practice.java
@@ -0,0 +1,11 @@
+package BookingInquiryPackage;
+
+public class Practice {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		Thread.sleep(-1000);
+      System.out.println("abc");
+	}
+
+}
